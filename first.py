1642ad6160de560703d418c2bc8039ef9122eb57
anushagi <anushagiri545@gmail.com>
2025-03-04 17:21:17 +0545

    some changes

--- first.py
+++ first.py
@@ -12,14 +12,23 @@ driver.get("https://www.saucedemo.com/")
 username=driver.find_element(By.XPATH,"//input[@id='user-name']")
 username.click()
 username.send_keys("standard_user")
-
 password=driver.find_element(By.XPATH,"//input[@id='password']")
 password.send_keys("secret_sauce")
-
 login=driver.find_element(By.XPATH,"//input[@id='login-button']")
 login.click()
+product=driver.find_element(By.XPATH,"//div[normalize-space()='Sauce Labs Onesie']")
+product.click()
+description=driver.find_element(By.XPATH,"//div[@class='inventory_details_desc large_size']")
+print(f"Description:{description.text}")
+price=driver.find_element(By.XPATH,"//div[@class='inventory_details_price']")
+print(f"Price:{price.text}")
+# print("Page Title:",driver.title)
 
 time.sleep(6)
-print("Page Title:",driver.title)
+
+
+
+
+
 driver.quit()
 
