d3eb0439f0af5accbfba07d2e0a343a062d52097
anushagi <anushagiri545@gmail.com>
2025-03-10 12:44:52 +0545

    few changes

--- setup/basetest.py
+++ setup/basetest.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from selenium import webdriver
@@ -9,11 +10,18 @@ class BaseTest:
     logger = logging.getLogger(__name__)
 
     def setup_method(self, method):
+        driver_path = r"C:\Users\anushagi\OneDrive - Infinite Computer Solutions (India) Limited\Documents\Class_automation\Anusha_automation\bin\chromedriver.exe"
 
-        self.driver = webdriver.Chrome()
+        ser = Service(driver_path)
+        logging.info("set up driver")
+        driver = webdriver.Chrome(service=ser)
+        self.driver=driver
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
 
+        creds_path = r"C:\Users\anushagi\OneDrive - Infinite Computer Solutions (India) Limited\Documents\Class_automation\Anusha_automation\cred\cred.json"
+        with open(creds_path, "r") as f:
+            self.cred = json.load(f)
 
     def teardown_method(self, method):
         self.driver.quit()
