3477262a1f61bf2538900b7c6f9d98cbf240e9fa
anushagi <anushagiri545@gmail.com>
2025-03-10 14:05:44 +0545

    few changes

--- testfile/test_login_pom.py
+++ testfile/test_login_pom.py
@@ -1,3 +1,5 @@
+
+
 import logging
 import time
 
@@ -5,7 +7,7 @@ from setup.basetest import BaseTest
 
 from page_objects.LoginPom.loginpage import LoginPage
 
-class TestLogs(BaseTest):
+class TestLogin(BaseTest):
     def test_login(self):
         url = self.cred["base_url"]
         self.driver.get(url)
@@ -16,3 +18,36 @@ class TestLogs(BaseTest):
         logging.info("logged in")
 
         time.sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from setup.basetest import BaseTest
+#
+# from page_objects.LoginPom.loginpage import LoginPage
+#
+# class TestLogin(BaseTest):
+#
+#     def test_login(self):
+#         url = self.cred["base_url"]
+#         self.driver.get(url)
+#         login_page = LoginPage(self.driver)
+#         uname = self.cred["standard_username"]
+#         pwd = self.cred["Password"]
+#         login_page.login(uname,pwd)
+#
+#
+#
+#
