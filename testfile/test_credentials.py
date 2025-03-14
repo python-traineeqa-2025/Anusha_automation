89adbee7dc2c85ae549c8e7668470fcfa4bce75b
anushagi <anushagiri545@gmail.com>
2025-03-14 13:45:13 +0545

    few changes

---
+++ testfile/test_credentials.py
@@ -0,0 +1,40 @@
+import logging
+import time
+
+from selenium.webdriver.common.by import By
+import json
+from setup.basetest import BaseTest
+
+class TestCred(BaseTest):
+    def test_cred(self):
+        self.driver.get("https://www.saucedemo.com/")
+        logging.info("opened the site")
+        cred_path = r"C:\Users\anushagi\OneDrive - Infinite Computer Solutions (India) Limited\Documents\Class_automation\Anusha_automation\cred\credlist.json"
+        with open(cred_path, 'r') as file:
+            cred = json.load(file)
+
+
+        for user, credentials in cred.items():
+            uname = credentials['uname']
+            pwd = credentials['pwd']
+
+            username = self.driver.find_element(By.XPATH, "//input[@id='user-name']")
+            password= self.driver.find_element(By.XPATH, "//input[@id='password']")
+            login = self.driver.find_element(By.XPATH, "//input[@id='login-button']")
+            username.send_keys(uname)
+            password.send_keys(pwd)
+            time.sleep(2)
+            login.click()
+            time.sleep(2)
+            self.driver.back()
+
+
+
+
+
+
+
+
+
+
+
